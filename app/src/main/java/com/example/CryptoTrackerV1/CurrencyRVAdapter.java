bb8561b0fc88d22735e996f89679ca020ce3d596
noahsmith774 <noah.smith774@my.simpson.edu>
2023-09-15 09:27:26 -0500

    Initial code for app

---
+++ app/src/main/java/com/example/CryptoTrackerV1/CurrencyRVAdapter.java
@@ -0,0 +1,75 @@
+package com.example.CryptoTrackerV1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+// creating adapter class and passing array list and View Holder class
+
+public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.CurrencyViewholder> {
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
+    private ArrayList<CurrencyModel> currencyModels;
+    private Context context;
+
+    public CurrencyRVAdapter(ArrayList<CurrencyModel> currencyModels, Context context) {
+        this.currencyModels = currencyModels;
+        this.context = context;
+    }
+
+    // method to filter list.
+    public void filterList(ArrayList<CurrencyModel> filterlist) {
+        // adding filtered list to our
+        // array list and notifying data set changed
+        currencyModels = filterlist;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public CurrencyRVAdapter.CurrencyViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // method to inflate the layout file created for recycler view
+        // on below line we are inflating our layout file.
+        View view = LayoutInflater.from(context).inflate(R.layout.currency_rv_item, parent, false);
+        return new CurrencyRVAdapter.CurrencyViewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CurrencyRVAdapter.CurrencyViewholder holder, int position) {
+        // on below line we are setting data to our item of
+        // recycler view and all its views.
+        CurrencyModel modal = currencyModels.get(position);
+        holder.nameTV.setText(modal.getName());
+        holder.rateTV.setText("$ " + df2.format(modal.getPrice()));
+        holder.symbolTV.setText(modal.getSymbol());
+    }
+
+    @Override
+    public int getItemCount() {
+        // on below line we are returning
+        // the size of our array list.
+        return currencyModels.size();
+    }
+
+    // on below line we are creating our view holder class
+    // which will be used to initialize each view of our layout file.
+    public class CurrencyViewholder extends RecyclerView.ViewHolder {
+        private TextView symbolTV, rateTV, nameTV;
+
+        public CurrencyViewholder(@NonNull View itemView) {
+            super(itemView);
+            // on below line we are initializing all
+            // our text views along with  its ids.
+            symbolTV = itemView.findViewById(R.id.idTVSymbol);
+            rateTV = itemView.findViewById(R.id.idTVRate);
+            nameTV = itemView.findViewById(R.id.idTVName);
+        }
+    }
+}
